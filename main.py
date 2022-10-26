ce770e401d07e08b71c891e3d586e080cbd06fd7
Ryan Bilash <rbilash@udel.edu>
2022-10-25 23:48:01 -0400

    Fix: Only select planets displayed

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import csv
 import matplotlib
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.widgets as widgets
+
 
 DEFAULT_COLOR = "#4682B4"
 
-COLOR_UNIQUE = True
+COLOR_KEY = "p"
 
 POSITION_SCALE = 1
 RADIUS = 0.25
@@ -69,6 +71,10 @@ def color_by_points(graph, point_color):
 
     return list_colors
 
+def color_by_control(graph, filename):
+    # TODO
+    return None
+
 
 def planet_pos(filename):
     file = open(filename)
@@ -87,11 +93,13 @@ def onclick(event):
     clicked = None
     smallest_distance = 999
     for key in pos:
+        if key not in G.nodes():
+            continue
         distance = ((x-pos[key][0]) ** 2) + ((y-pos[key][1]) ** 2)
         if distance < RADIUS and key is None:
             clicked = key
             smallest_distance = distance
-        elif key is not None:
+        elif distance < RADIUS and key is not None:
             if distance < smallest_distance:
                 clicked = key
                 smallest_distance = distance
@@ -106,15 +114,24 @@ def onclick(event):
 
 
 def draw_graph(change_colors=False, change_edges=False):
-    global fig
+    global fig, colors
     fig.clf()
     plt.clf()
 
     # nodes
     if change_colors:
-        global colors1
+        global colors1, colors2
         colors1 = color_by_points(G, full_colors)
-    nx.draw_networkx_nodes(G, pos, node_size=500, node_color=colors1)
+        colors2 = color_map_to_list(full_colors, G)
+        match COLOR_KEY:
+            case 'p':
+                colors = colors1
+            case 'u':
+                colors = colors2
+            case 'c':
+                colors = colors3
+
+    nx.draw_networkx_nodes(G, pos, node_size=500, node_color=colors)
 
     # edges
     if change_edges:
@@ -126,6 +143,7 @@ def draw_graph(change_colors=False, change_edges=False):
     nx.draw_networkx_labels(G, label_pos, font_size=8, font_family="sans-serif")
 
     ax = plt.gca()
+    #widgets.CheckButtons(ax,["Core","Rim","Unknown"])
     ax.margins(0.1)
     plt.axis("off")
     plt.tight_layout()
@@ -134,20 +152,24 @@ def draw_graph(change_colors=False, change_edges=False):
     fig.canvas.draw()
 
 
+def func(checkbox):
+    if checkbox:
+        print("here")
+
 if __name__ == '__main__':
     G = nx.Graph()
     fig = plt.figure("Map", figsize=(10, 10))
 
     add_hyperlanes(G, "data/hyperlanes/limited.csv")
     add_hyperlanes(G, "data/hyperlanes/merc.csv")
-    # add_hyperlanes(G, "data/hyperlanes/deep-core.csv")
-    # add_hyperlanes(G, "data/hyperlanes/core-inner.csv")
-    add_hyperlanes(G, "data/hyperlanes/inner-rim.csv")
-    # add_hyperlanes(G, "data/hyperlanes/hapan-space.csv")
-    # add_hyperlanes(G, "data/hyperlanes/northern.csv")
-    # add_hyperlanes(G, "data/hyperlanes/slice.csv")
-    # add_hyperlanes(G, "data/hyperlanes/western-reaches.csv")
-    # add_hyperlanes(G, "data/hyperlanes/unknown.csv")
+    #add_hyperlanes(G, "data/hyperlanes/deep-core.csv")
+    #add_hyperlanes(G, "data/hyperlanes/core-inner.csv")
+    #add_hyperlanes(G, "data/hyperlanes/inner-rim.csv")
+    #add_hyperlanes(G, "data/hyperlanes/hapan-space.csv")
+    #add_hyperlanes(G, "data/hyperlanes/northern.csv")
+    #add_hyperlanes(G, "data/hyperlanes/slice.csv")
+    #add_hyperlanes(G, "data/hyperlanes/western-reaches.csv")
+    #add_hyperlanes(G, "data/hyperlanes/unknown.csv")
 
     pos = planet_pos("data/planet-loc.csv")
     label_pos = {}
@@ -162,7 +184,8 @@ if __name__ == '__main__':
 
     colors1 = color_by_points(G, full_colors)
     colors2 = define_colors(G, "data/colors-u.csv")
-    # colors3 = color_by_control(G, get_general_map("data/player-control.csv"))
+    colors3 = color_by_control(G, "data/player-control.csv")
+    colors = colors1
 
     draw_graph()
     plt.show()
